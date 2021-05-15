cb3b98e3256efb428a8be5a1817d43a660fb2b48
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-05-14 23:55:56 -0500

    fontawesome issue solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className='container'>
         <a className="navbar"> </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>
+            <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>        
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
